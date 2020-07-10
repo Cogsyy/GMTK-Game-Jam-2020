8bc4366280218d3616e27372318c99aa4713f80f
Jordan <jordancog94@gmail.com>
2020-07-10 18:39:17 -0400

    Added gamecontroller and messagesbroker, as well as Singleton class

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : Singleton<GameController>
+{
+    public static GameController instance;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Reuseable/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T Instance { get; private set; }
+
+    protected virtual void Awake()
+    {
+        InitSingleton(this as T);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        Instance = null;
+    }
+
+    private void InitSingleton(T newInstance)
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = newInstance;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
