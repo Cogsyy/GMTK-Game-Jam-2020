ff5a90dd5082ee3e69c1d4e8b8042317fdc72ffd
Jordan <jordancog94@gmail.com>
2020-07-12 14:18:19 -0400

    added game icon, added dog barks, removed collision

---
+++ Assets/Scripts/DogBarks.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogBarks : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _level1Sequence;
+    [SerializeField] private List<GameObject> _level5Sequence;
+
+    [SerializeField] private GameObject _barkbark;
+    [SerializeField] private GameObject _bark;
+    [SerializeField] private GameObject _removeChip;
+
+    private void DisableAll()
+    {
+        _barkbark.SetActive(false);
+        _bark.SetActive(false);
+        _removeChip.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    public void PlayLevel1Sequence()
+    {
+        DisableAll();
+        gameObject.SetActive(true);
+        StartCoroutine(SequenceCo(_level1Sequence));
+    }
+
+    public void PlayLevel5Sequence()
+    {
+        DisableAll();
+        gameObject.SetActive(true);
+        StartCoroutine(SequenceCo(_level5Sequence));
+    }
+
+    private IEnumerator SequenceCo(List<GameObject> sequence)
+    {
+        for (int i = 0; i < sequence.Count; i++)
+        {
+            sequence[i].SetActive(true);
+            yield return new WaitForSeconds(2);
+            sequence[i].SetActive(false);
+        }
+
+        DisableAll();
+    }
+}
