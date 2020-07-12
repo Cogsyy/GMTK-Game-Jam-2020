3bf4e9839ebe5672ef5b5d9f7c1800d236ca822f
mgooch <michael@mgooch.com>
2020-07-11 20:08:49 -0400

    Added a static transition

---
+++ Assets/Scripts/StaticTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject _staticObject;
+    [SerializeField] private AudioSource _audioSource;
+
+    void Start()
+    {
+        _staticObject.SetActive(false);
+    }
+
+    public void TriggerStaticTransition()
+    {
+        StartCoroutine(TriggerTransition());
+    }
+
+    private IEnumerator TriggerTransition()
+    {
+        _staticObject.SetActive(true);
+        _audioSource.volume = 1;
+        yield return new WaitForSeconds(0.1f);
+        _staticObject.SetActive(false);
+        _audioSource.volume = 0;
+    }
+}
